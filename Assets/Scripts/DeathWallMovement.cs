c27f88c82ee258ca1328721d90fc792bd1a68b47
Patrick <prm5983@rit.edu>
2020-01-25 15:44:47 -0500

    Added death wall that moves, added death (loading active scene again)

---
+++ Assets/Scripts/DeathWallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathWallMovement : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
